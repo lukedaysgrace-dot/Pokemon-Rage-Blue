a54c6297ec90699a2b96eb17181b39ba3a38f9a0
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-05-22 21:54:56 -0500

    no wild encounters first 2 floors tower

--- data/wild/maps/PokemonTower1F.asm
+++ data/wild/maps/PokemonTower1F.asm
@@ -1,36 +1,6 @@
 PokemonTower1FWildMons:
-IF DEF(_RED)
-	def_grass_wildmons 8 ; encounter rate
-	db  15, GASTLY
-	db  16, GASTLY
-	db  17, GASTLY
-	db   17, CUBONE
-	db  14, GASTLY
-	db  13, CUBONE
-	db   17, GASTLY
-	db  15, ZUBAT
-	db   17, GASTLY
-	db   17, ZUBAT
+	def_grass_wildmons 0 ; encounter rate
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
-ENDC
-
-IF DEF(_BLUE)
-	def_grass_wildmons 8 ; encounter rate
-	db  15, GASTLY
-	db  16, GASTLY
-	db  17, DUSKULL
-	db   17, CUBONE
-	db  14, DUSKULL
-	db  13, CUBONE
-	db   17, MISDREAVUS
-	db  15, ZUBAT
-	db   17, GASTLY
-	db   17, ZUBAT
-	end_grass_wildmons
-
-	def_water_wildmons 0 ; encounter rate
-	end_water_wildmons
-ENDC
--- data/wild/maps/PokemonTower2F.asm
+++ data/wild/maps/PokemonTower2F.asm
@@ -1,37 +1,6 @@
 PokemonTower2FWildMons:
-IF DEF(_RED)
-	def_grass_wildmons 10 ; encounter rate
-	db  17, GASTLY
-	db  18, GASTLY
-	db   19, GASTLY
-	db   19, CUBONE
-	db  16, ZUBAT
-	db  15, GASTLY
-	db   19, CUBONE
-	db   19, ZUBAT
-	db   19, GASTLY
-	db   19, CUBONE
+	def_grass_wildmons 0 ; encounter rate
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
-ENDC
-
-IF DEF(_BLUE)
-	def_grass_wildmons 10 ; encounter rate
-	db  17, GASTLY
-	db  18, GASTLY
-	db   19, GASTLY
-	db   19, CUBONE
-	db  16, DUSKULL
-	db  15, DUSKULL
-	db   19, CUBONE
-	db   19, ZUBAT
-	db   19, GASTLY
-	db   19, CUBONE
-	end_grass_wildmons
-
-	def_water_wildmons 0 ; encounter rate
-	end_water_wildmons
-ENDC
-
