1f3cacf378a84929003f40db7e8702ad8fbc41c4
lukedaysgrace-dot <lukedaysgrace@hotmail.com>
2026-05-26 12:28:13 -0500

    increased trainer payout

--- data/trainers/pic_pointers_money.asm
+++ data/trainers/pic_pointers_money.asm
@@ -7,22 +7,22 @@ TrainerPicAndMoneyPointers::
 	table_width 5
 	; pic pointer, base reward money
 	; money received after battle = base money × level of last enemy mon
-	pic_money YoungsterPic,    1500
-	pic_money BugCatcherPic,   1000
-	pic_money LassPic,         1500
+	pic_money YoungsterPic,    2000
+	pic_money BugCatcherPic,   2000
+	pic_money LassPic,         2000
 	pic_money SailorPic,       3000
-	pic_money JrTrainerMPic,   2000
-	pic_money JrTrainerFPic,   2000
+	pic_money JrTrainerMPic,   2500
+	pic_money JrTrainerFPic,   2500
 	pic_money PokemaniacPic,   5000
 	pic_money SuperNerdPic,    2500
 	pic_money HikerPic,        3500
-	pic_money BikerPic,        2000
+	pic_money BikerPic,        3000
 	pic_money BurglarPic,      9000
 	pic_money EngineerPic,     5000
 	pic_money ArianaPic,       4500
 	pic_money FisherPic,       3500
-	pic_money SwimmerPic,      2000
-	pic_money SwimmerFPic,     2000
+	pic_money SwimmerPic,      3000
+	pic_money SwimmerFPic,     3000
 	pic_money CueBallPic,      2500
 	pic_money GamblerPic,      7000
 	pic_money BeautyPic,       7000
@@ -32,7 +32,7 @@ TrainerPicAndMoneyPointers::
 	pic_money TamerPic,        4000
 	pic_money BirdKeeperPic,   2500
 	pic_money BlackbeltPic,    2500
-	pic_money Rival1Pic,       3500
+	pic_money Rival1Pic,       4500
 	pic_money ProfOakPic,      9900
 	pic_money ChiefPic,        3000
 	pic_money ScientistPic,    5000
